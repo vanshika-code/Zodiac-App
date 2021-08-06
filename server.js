c256b5a4f5020c3f97916ae643c87ea5ae7365d4
vanshia gupta <vanshika-code>
2021-08-07 00:15:38 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app=express();
+app.use(express.static(__dirname + '/client'));
+const port= process.env.PORT || 3000;
+app.listen(port,()=>{
+	console.log('Server is running on port '+port);
+});
+
